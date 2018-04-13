4fc014cb6a03f92504a8be994ae8b755180bf286
Kresimir rakic <kresimir.rakic@fsre.sum.ba>
2018-04-13 06:11:19 +0200

    abc

--- TestRepository2/Program.cs
+++ TestRepository2/Program.cs
@@ -9,7 +9,7 @@ namespace TestRepository2
             Console.WriteLine("Novi repozitorij!");
             Console.WriteLine("I nova izmjena u njemu.");
             Console.WriteLine("I nova grana");
-            Console.WriteLine("Ajde");
+            Console.WriteLine("Ajde!");
         }
     }
 }
