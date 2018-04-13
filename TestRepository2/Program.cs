97d8233603d138a6460e4e52fe8cc8402a3d1d0a
Kresimir rakic <kresimir.rakic@fsre.sum.ba>
2018-04-13 06:02:06 +0200

    remote branch

--- TestRepository2/Program.cs
+++ TestRepository2/Program.cs
@@ -8,6 +8,7 @@ namespace TestRepository2
         {
             Console.WriteLine("Novi repozitorij!");
             Console.WriteLine("I nova izmjena u njemu.");
+            Console.WriteLine("I nova grana");
         }
     }
 }
